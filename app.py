db9e6c1fad4c325a8e1991ac3a1533af11a7ee0b
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2024-04-22 16:37:39 -0400

    INTRO TO JWT

--- app.py
+++ app.py
@@ -1,29 +1,111 @@
+import os
 from flask import Flask, request, jsonify
-from models import db, Task
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
+from flask_cors import CORS
+from models import db, Task, User
+from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
+
+load_dotenv()
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
 #app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:root@localhost:3306/dbft24"
 db.init_app(app)
+jwt = JWTManager(app)
+CORS(app)
+
+
+@app.route('/token/<email>', methods=['GET'])
+def token(email):
+
+    data = {
+        "access_token": create_access_token(identity=email)
+    }
+
+    return jsonify(data), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not email:
+        return jsonify({ "message": "Email is required"}), 400
+    
+    if not password:
+        return jsonify({ "message": "Password is required"}), 400
+    
+    userFound = User.query.filter_by(email=email).first()
+
+    if not userFound:
+        return jsonify({ "message": "email/password are incorrects"}), 401
+    
+    if not check_password_hash(userFound.password, password):
+        return jsonify({ "message": "email/password are incorrects"}), 401
+    
+    access_token = create_access_token(identity=userFound.id)
+    datos = {
+        "access_token": access_token,
+        "user": userFound.serialize()
+    }
+    return jsonify(datos), 200
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not email:
+        return jsonify({ "message": "Email is required"}), 400
+    
+    if not password:
+        return jsonify({ "message": "Password is required"}), 400
+    
+    userFound = User.query.filter_by(email=email).first()
+    if userFound:
+        return jsonify({"message": "Email already in use"}), 400
+
+
+    user = User(email=email, password=generate_password_hash(password))
+    user.save()
+
+    if user:
+        access_token = create_access_token(identity=user.id)
+        datos = {
+            "access_token": access_token,
+            "user": user.serialize()
+        }
+        return jsonify(datos), 201
+
+
 
 
 @app.route('/todos', methods=['GET'])
+@jwt_required() # Private Route
 def get_all_todos():
+    email = get_jwt_identity()
+    print(email)
     todos = Task.query.all() # [<object 0x4434343>, <object 0x303030300>]
     todos = list(map(lambda task: task.serialize(), todos)) # [{id: 1, description: 'Comprar Pan', done: False }]
     return jsonify(todos), 200
 
 
 @app.route('/todos', methods=['POST'])
+@jwt_required() # Private Route
 def add_task():
+    id = get_jwt_identity()
     task_info = request.json
     if not 'description' in task_info:
         return jsonify({ "msg": "Description is required"}), 400
     if task_info["description"] == "":
         return jsonify({ "msg": "Description is required"}), 400
     
-    task = Task(description=task_info["description"]) # Creando la tarea
+    task = Task(description=task_info["description"], users_id=id) # Creando la tarea
     db.session.add(task) # Ejecutando el INSERT
     db.session.commit() # Guardando los Cambios
 
@@ -31,7 +113,9 @@ def add_task():
 
 
 @app.route('/todos/<int:id>/complete', methods=['GET'])
+@jwt_required() # Private Route
 def complete_task(id):
+    id = get_jwt_identity()
     task = Task.query.get(id)
 
     if not task:
@@ -44,8 +128,13 @@ def complete_task(id):
 
 
 @app.route('/todos/<int:id>/delete', methods=['DELETE'])
+@jwt_required() # Private Route
 def delete_task(id):
+    users_id = get_jwt_identity()
     task = Task.query.get(id)
+
+    if task.users_id != users_id:
+        return jsonify({ "message": "You can't delete this task. You are not owner"}), 400
 
     if not task:
         return jsonify({ "msg": f"Task with id: {id} not found"}), 404
--- models.py
+++ models.py
@@ -1,15 +1,36 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
 class Task(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(255), nullable=False)
     done = db.Column(db.Boolean, default=False)
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship("User")
 
     def serialize(self):
         return {
             "id": self.id,
             "description": self.description,
-            "done": self.done
+            "done": self.done,
+            "user": self.user.email
         }
